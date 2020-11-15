373476f65a3483a0ea18671ac0804c872e669689
diggerb <james@briant.co.uk>
2020-11-15 16:06:43 +0000

    Added summary

--- src/Coursework1/main/Q1c.cpp
+++ src/Coursework1/main/Q1c.cpp
@@ -10,14 +10,29 @@ double f(double x);
 
 int main(int argc, char* argv[])
 {
-    int n = 5;
-    int N = 2*n + 1;
+    int n1 = 5;
+    //int N1 = 2*n1 + 1;
+    int n2 = 10;
+    //int N2 = 2*n2 + 1;
+    int n3 = 20;
+    //int N3 = 2*n3 + 1;
+
     double a = 0.0;
     double b = 1.0;
 
-    double approximation = ApplySimpsonsRule(n, a, b, &f);
+    double I1 = ApplySimpsonsRule(n1, a, b, &f);
+    double I2 = ApplySimpsonsRule(n2, a, b, &f);
+    double I3 = ApplySimpsonsRule(n3, a, b, &f);
 
-    std::cout << 1/approximation;
+    double I0 = 1/ M_PI;
+
+    std::cout << "n = " << n1 << ", error = " << fabs(I1 - I0) << std::endl;
+    std::cout << "n = " << n2 << ", error = " << fabs(I2 - I0) << std::endl;
+    std::cout << "n = " << n3 << ", error = " << fabs(I3 - I0) << std::endl;
+
+    std::cout << "All estimates are very accurate with the worst estimate only";
+    std::cout << " differing from the true solution at the 5th decimal place. ";
+    std::cout << "This shows ";
 
     return 0;
 }
