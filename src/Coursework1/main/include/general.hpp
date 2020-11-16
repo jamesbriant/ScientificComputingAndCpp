df2af83109c01a23b8fb8b87754040680684c284
diggerb <james@briant.co.uk>
2020-11-16 14:09:33 +0000

    Added additional functions

--- src/Coursework1/main/include/general.hpp
+++ src/Coursework1/main/include/general.hpp
@@ -5,9 +5,14 @@ void CopyVector(int length, double* pVec, double* pNewVec);
 void PrintVector(int length, double* pVec);
 void PrintMatrix(int p, int q, double** pMatrix);
 double** AllocateMatrix(int nrows, int ncols);
+double** AllocateTridiagonalMatrix(int nrows);
 void PopulateMatrix(int nrows, int ncols, double** pMatrix);
+void PopulateVector0(int nrows, double* pVector);
 double* Multiply(int nrows, int ncols, double** pM, double* px);
 double* Multiply(int nrows, int ncols, double* px, double** pM);
+double* MultiplyTridiagonalMatrix(int nrows, double** pM, double* px);
+double* Add(int n, double* a, double* b);
+double* Subtract(int n, double* a, double* b);
 void DeallocateMatrix(int nrows, double** pMatrix);
 
 #endif
--- src/Coursework1/main/source/general.cpp
+++ src/Coursework1/main/source/general.cpp
@@ -34,7 +34,7 @@ void PrintMatrix(int p, int q, double** pMatrix)
 }
 
 double** AllocateMatrix(int nrows, int ncols)
-// Alocates matrix of size nrows x ncols
+// Allocates matrix of size nrows x ncols
 // Note this does NOT populate the matrix
 {
     double** p_A;
@@ -43,6 +43,22 @@ double** AllocateMatrix(int nrows, int ncols)
     {
         p_A[i] = new double [ncols];
     }
+
+    return p_A;
+}
+
+double** AllocateTridiagonalMatrix(int nrows)
+// Alloccates matrix of size nrows x 3, but with first and last row of length 2
+// Note this does NOT populate the matrix
+{
+    double** p_A;
+    p_A = new double* [nrows];
+    p_A[0] = new double [2];
+    for(int i=1; i<nrows-1; i++)
+    {
+        p_A[i] = new double [3];
+    }
+    p_A[nrows-1] = new double [2];
 
     return p_A;
 }
@@ -57,6 +73,16 @@ void PopulateMatrix(int nrows, int ncols, double** pMatrix)
         {
             pMatrix[i][j] = ((double) rand() / (RAND_MAX));
         }
+    }
+}
+
+void PopulateVector0(int nrows, double* pVector)
+// Populates the vector with zeros
+// Requires a predefined vector as an input
+{
+    for(int i=0; i<nrows; i++)
+    {
+        pVector[i] = 0;
     }
 }
 
@@ -102,6 +128,59 @@ double* Multiply(int nrows, int ncols, double* px, double** pM)
     }
 
     return p_xM;
+}
+
+double* MultiplyTridiagonalMatrix(int nrows, double** pM, double* px)
+// returns the matrix product of tridiagonal matrix M and a vector x, Mx
+{
+    // instantiate output array
+    double* p_Mx;
+    p_Mx = new double[nrows];
+
+    // calculate end points of output array 
+    p_Mx[0] = pM[0][0]*px[0] + pM[0][1]*px[1];
+    p_Mx[nrows-1] = pM[nrows-1][0]*px[nrows-2] + pM[nrows-1][1]*px[nrows-1];
+
+    // calculate middle points of output array
+    for(int i=1; i<nrows-1; i++)
+    {
+        double sum = 0;
+
+        for(int j=0; j<3; j++)
+        {
+            sum += pM[i][j] * px[i-1+j]; //adjusts for 0's in the tridiagonal
+        }
+
+        p_Mx[i] = sum;
+    }
+
+    return p_Mx;
+}
+
+double* Add(int n, double* a, double* b)
+// Adds vector b to vector a
+{
+    double* sum;
+    sum = new double[n];
+    for(int i=0; i<n; i++)
+    {
+        sum[i] = a[i] + b[i];
+    }
+
+    return sum;
+}
+
+double* Subtract(int n, double* a, double* b)
+// Subtracts vector b from vector a
+{
+    double* diff;
+    diff = new double[n];
+    for(int i=0; i<n; i++)
+    {
+        diff[i] = a[i] - b[i];
+    }
+
+    return diff;
 }
 
 void DeallocateMatrix(int nrows, double** pMatrix)
