97e9893cbf369f06c6bbe911e5246b4f59e8bbe2
diggerb <james@briant.co.uk>
2020-11-24 18:31:38 +0000

    Add comments and change include directory

--- src/Coursework1/main/Q1c.cpp
+++ src/Coursework1/main/Q1c.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 #include <cmath>
-#include "include/general.hpp"
-#include "include/quadrature.hpp"
+#include "general.hpp"
+#include "quadrature.hpp"
 
 double f(double x);
 
-// I compile this by using 
-// `g++ Q1c.cpp source/general.cpp source/quadrature.cpp -o Q1c.exe`
-
 int main(int argc, char* argv[])
 {
+    // Define question parameters
     int n5 = 5;
     int n10 = 10;
     int n20 = 20;
@@ -17,24 +15,32 @@ int main(int argc, char* argv[])
     double a = 0.0;
     double b = 1.0;
 
+    // Call the integration algorithm
     double I5 = ApplySimpsonsRule(n5, a, b, &f);
     double I10 = ApplySimpsonsRule(n10, a, b, &f);
     double I20 = ApplySimpsonsRule(n20, a, b, &f);
 
+    // Analytical (true) solution
     double I0 = 1/ M_PI;
 
-    std::cout << "n = " << n5 << ", error = " << fabs(I5 - I0) << std::endl;
-    std::cout << "n = " << n10 << ", error = " << fabs(I10 - I0) << std::endl;
-    std::cout << "n = " << n20 << ", error = " << fabs(I20 - I0) << std::endl;
+    // Calculate and print the errors
+    std::cout << "n=" << n5 << ", Approximation=" << I5;
+    std::cout << ", Error=" << fabs(I5 - I0) << std::endl;
+    std::cout << "n=" << n10 << ", Approximation=" << I10;
+    std::cout << ", Error=" << fabs(I10 - I0) << std::endl;
+    std::cout << "n=" << n20 << ", Approximation=" << I20;
+    std::cout << ", Error=" << fabs(I20 - I0) << std::endl;
 
     std::cout << "All estimates are very accurate with the worst estimate only";
     std::cout << " differing from the true solution at the 5th decimal place. ";
-    std::cout << "This shows ";
+    std::cout << "Increasing the number of nodal points decreases the error ";
+    std::cout << "as expected.";
 
     return 0;
 }
 
 double f(double x)
+// locally defined function specific to this question
 {
     return x*sin(M_PI * x);
 }
