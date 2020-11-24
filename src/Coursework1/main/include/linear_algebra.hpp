df7295b544a011b45ebfb2e6ceb68b5278232447
diggerb <james@briant.co.uk>
2020-11-24 18:30:19 +0000

    remove white space

--- src/Coursework1/main/include/linear_algebra.hpp
+++ src/Coursework1/main/include/linear_algebra.hpp
@@ -4,9 +4,9 @@
 void CalculateR0(int n, double* pr0, double* pb, double** pA, double* px0);
 bool CheckEarlyBreak(int n, double* pb, double** pA, double* ph, double tol);
 void ApplyBiGCstab(int n, double* pxEstimate, double** pA, double* px0, 
-                        double* pb, double tol, int maxIter);
+        double* pb, double tol, int maxIter);
 void CalculateP(int n, double* pp, double* pr, double* pNu, double beta, 
-                double omega);
+        double omega);
 void CalculateH(int n, double* ph, double* px, double alpha, double* pp);
 void CalculateS(int n, double* ps, double* pr, double alpha, double* pNu);
 void CalculateX(int n, double* px, double* ph, double omega, double* ps);
