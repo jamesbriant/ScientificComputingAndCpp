6bd3b5ca71378e9056e609b4bddcf3297b68efd2
diggerb <james@briant.co.uk>
2020-11-24 18:30:39 +0000

    Add new prototypes

--- src/Coursework1/main/include/quadrature.hpp
+++ src/Coursework1/main/include/quadrature.hpp
@@ -3,6 +3,10 @@
 
 double** GenerateSimpsonsMesh(int n, double a, double b);
 double ApplySimpsonsRule(int n, double a, double b, 
-                         double (*pFunction)(double x));
+            double (*pFunction)(double x));
+double ApplySimpsonsRule(int n, double a, double b,
+            double (*pIntegrand)(double x, double h, double a, 
+                double (*pUserFunction)(double x)),
+            double (*pUserFunction)(double x));
 
 #endif
